2afce994c555c020a9fe136430a20d14fab3ff45
DirkFriedenberger <DirkFriedenberger@deutschebahn.com>
2020-09-10 18:29:51 +0200

    read and write to device

--- connect-arduino/Connect.java
+++ connect-arduino/Connect.java
@@ -1,19 +1,62 @@
 // Java Program to illustrate reading from FileReader 
 // using BufferedReader 
 import java.io.*; 
-public class Connect 
+public class Connect extends Thread
 { 
+
+    private final String device;
+    public Connect(String device) 
+    { 
+        this.device =device;
+    }
+
+    public void run()
+    {
+        File file = new File(device); 
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file)); 
+
+            String st; 
+            while ((st = br.readLine()) != null) 
+                System.out.println(st); 
+            
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        finally 
+        {
+            if(br != null)
+            try {
+                	br.close();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String cmd) {
+        try {
+        /* Process p = */ Runtime.getRuntime().exec(new String[]{"echo", cmd, device });
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args)throws Exception 
     { 
-    // We need to provide file path as the parameter: 
-    // double backquote is to avoid compiler interpret words 
-    // like \test as \t (ie. as a escape sequence) 
-    File file = new File("/dev/ttyACM0"); 
+       Connect connect = new Connect("/dev/ttyACM0");
+       connect.start();
+       while(true)
+       {
+          Thread.sleep(10000);
+          connect.send("ON");
+          Thread.sleep(10000);
+          connect.send("OFF");
+       }
+    }
 
-    BufferedReader br = new BufferedReader(new FileReader(file)); 
-
-    String st; 
-    while ((st = br.readLine()) != null) 
-        System.out.println(st); 
-    } 
+   
 } 
